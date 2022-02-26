f591195e5adc66c37e3bcc98ce128d24d7bb08b2
Akshaylal <akshaylals2000@gmail.com>
2022-02-26 17:13:35 +0530

    clean up

--- aht2x.cpp
+++ aht2x.cpp
@@ -6,13 +6,13 @@ AHT2x::AHT2x(i2c_inst *i2cInst, uint16_t address = 0x38){
 }
 
 float AHT2x::temperature(){
-    this->measure();
-    return this->t;
+    measure();
+    return t;
 }
 
 float AHT2x::humidity(){
-    this->measure();
-    return this->h;
+    measure();
+    return h;
 }
 
 void AHT2x::measure(){
@@ -20,24 +20,24 @@ void AHT2x::measure(){
     buff[1] = 0x33; // command parameters
     buff[2] = 0x00; // command parameters
 
-    i2c_write_blocking(this->i2cInst, this->address, this->buff, 3, false);
+    i2c_write_blocking(i2cInst, address, buff, 3, false);
     
     sleep_ms(80); // wait 80ms to complete measurement
     
-    while(this->isBusy()){  // wait if busy
+    while(isBusy()){  // wait if busy
         sleep_ms(10);
     }
     
-    i2c_read_blocking(this->i2cInst, this->address, this->buff, 6, false);
+    i2c_read_blocking(i2cInst, address, buff, 6, false);
     
-    this->h = (this->buff[1] << 12) | (this->buff[2] << 4) | (this->buff[3] >> 4);
-    this->t = ((this->buff[3] & 0xF) << 16) | (this->buff[4] << 8) | (this->buff[5]);
+    h = (buff[1] << 12) | (buff[2] << 4) | (buff[3] >> 4);
+    t = ((buff[3] & 0xF) << 16) | (buff[4] << 8) | (buff[5]);
 
-    this->h = (this->h / 0x100000) * 100;
-    this->t = ((this->t / 0x100000) * 200) - 50;
+    h = (h / 0x100000) * 100;
+    t = ((t / 0x100000) * 200) - 50;
 }
 
 bool AHT2x::isBusy(){
-    i2c_read_blocking(this->i2cInst, this->address, this->buff, 1, false);
-    return (this->buff[0] >> 7) & 0x1;
+    i2c_read_blocking(i2cInst, address, buff, 1, false);
+    return (buff[0] >> 7) & 0x1;
 }
